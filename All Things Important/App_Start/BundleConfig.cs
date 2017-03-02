9048fe682da9a2446d24151b86e95c602e44e208
Captain Peanut <capnpeanut@gmail.com>
2017-03-01 20:09:28 -0500

    Newspaper color change

--- All Things Important/App_Start/BundleConfig.cs
+++ All Things Important/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace All_Things_Important
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/journal.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
